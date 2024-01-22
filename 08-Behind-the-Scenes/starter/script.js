2eb30272ea68931e4cf9a7413255599edf3f8d0f
Thevi <Thevi@DESKTOP-VK9STPU>
2024-01-22 20:59:57 +0100

    Testing the functionality of primitives and objects in js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -31,3 +31,20 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Thevi',
+  age: 23,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend: ', friend);
+console.log('Me', me);
