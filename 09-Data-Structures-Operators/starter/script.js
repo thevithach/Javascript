d483337dbdfa0069092cb5fdfa3375b5608aa232
thevithach <vitt@uia.no>
2024-01-26 22:52:52 +0100

    More strings :===)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,59 @@ const restaurant = {
   },
 };
 
+console.log('a+very+nice+string'.split('+'));
+console.log('Thevi Thach'.split(' '));
+
+const [firstName, lastName] = 'Thevi Thach'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  // for(const n of names) {
+  //   namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  // }
+
+  for (const n of names) {
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('thevi thach');
+
+// Padding - add a number of characters to a string until the string has a certain desired length
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(35, '+'));
+console.log('Thevi'.padStart(25, '+').padEnd(35, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(1234567890));
+console.log(maskCreditCard('1234567890'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
@@ -110,6 +163,7 @@ const checkBaggage = function (items) {
 checkBaggage('I have a laptop, some Food, and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a GUN for protection');
+*/
 /*
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
